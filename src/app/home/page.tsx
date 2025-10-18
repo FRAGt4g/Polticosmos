7b872d799ebd945716bca35c903c5bd772aafa64
Miles F <miles.fritzmather@gmail.com>
2025-10-18 18:45:31 -0500

    removed testing code

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,28 +1,7 @@
-"use client";
-
-import { useEffect } from "react";
-import { useCosmosContext } from "~/components/providers/cosmos-provider";
 import Title from "~/components/Title";
-import { createDummyBillInfo } from "~/lib/utils";
 import ThreeJSScene from "./ThreeJSScene";
 
 const Page = () => {
-  const { setSelectedStar } = useCosmosContext();
-
-  useEffect(() => {
-    function handleKeyPress(event: KeyboardEvent) {
-      if (event.key === " ") {
-        setSelectedStar(createDummyBillInfo());
-      }
-    }
-
-    document.addEventListener("keypress", handleKeyPress);
-
-    return () => {
-      document.removeEventListener("keypress", handleKeyPress);
-    };
-  }, []);
-
   return (
     <>
       <ThreeJSScene />
