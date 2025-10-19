ae5d15d42ad2546fff1097a5d32041d0427ab8b9
Malcolm Roalson <mtroalson@gmail.com>
2025-10-19 00:43:21 -0500

    add .gitignore

--- dist/sqlTypes.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=sqlTypes.js.map
--- dist/writeBillToSQL.d.ts
+++
@@ -1,4 +0,0 @@
-import type { BillType } from "./sqlTypes.js";
-import { type Pool } from 'pg';
-export declare function writeBillToSQL(pool: Pool, congress: number, type: BillType, number: number): Promise<void>;
-//# sourceMappingURL=writeBillToSQL.d.ts.map
