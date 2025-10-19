c3fa2eb9f1f9b0f9d4fb7f1db63b912fc663cd6e
Malcolm Roalson <mtroalson@gmail.com>
2025-10-19 13:30:28 -0500

    localhost IP

--- src/app/api/api-utils.ts
+++ src/app/api/api-utils.ts
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 
 const db = new Pool({
-  host: "localhost",//"75.81.64.177",
+  host: "172.20.0.2",//"75.81.64.177",
   port: 5432,
   user: "postgres",
   password: "dev",
